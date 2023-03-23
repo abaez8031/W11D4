cb07425ac94ce8cf59f7b3e5f1c26268261c3e93
Shan Kim <shankim35@gmail.com>
2023-03-23 15:18:00 -0400

    phase 2

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/index.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './Store/index.js';
 import './index.css';
 import App from './App';
+import { populateProduce } from './Store/produce.js';
+
+const store = configureStore();
 
 function Root() {
+  if (process.env.NODE_ENV !== "production") {
+    window.store = store;
+    window.populateProduce = populateProduce;
+  }
   return (
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
   );
 }
 
